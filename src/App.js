32fb6236e1738e263d3245fdd3f771ed2092f049
Jonathan Willingham <jwillingham1167@gmail.com>
2018-07-21 12:22:26 -0400

    hover pseudo selector with radium

--- src/App.js
+++ src/App.js
@@ -52,7 +52,11 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            }
         };
 
         let persons = null;
@@ -75,6 +79,10 @@ class App extends Component {
             ); // this.state.persons = person in the function
 
             style.backgroundColor = 'red';
+            style[':hover'] = {
+                backgroundColor: 'salmon',
+                color: 'black'
+            }
 
         }
         console.log(this.state.persons);
