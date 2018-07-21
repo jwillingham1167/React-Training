dcc445f9657699a660be69a11ee2ba3171a95f1c
Jonathan Willingham <jwillingham1167@gmail.com>
2018-07-21 11:09:24 -0400

    updating to remove the console warnings

--- src/App.js
+++ src/App.js
@@ -19,14 +19,23 @@ class App extends Component {
         this.setState({persons: persons});
     }
 
-    nameChangedHandler = (event) => {
-        this.setState({
-            persons: [
-                { name: 'Max', age: 28 },
-                { name: event.target.value, age: 29 },
-                { name: 'Stephanie', age: 26 }
-            ]
-        })
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        });
+        
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+
+        // const person = Object.assign({}, this.state.persons[personIndex]);
+
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState({persons: persons})
     }
 
     togglePersonsHandler = () => {
@@ -52,7 +61,12 @@ class App extends Component {
                 <div>
                     {
                         this.state.persons.map((person, index) => {
-                            return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id} />
+                            return <Person
+                                click={() => this.deletePersonHandler(index)}
+                                name={person.name}
+                                age={person.age}
+                                key={person.id} 
+                                changed={(event) => this.nameChangedHandler(event, person.id)} />
                         })
                     }
                 </div>
